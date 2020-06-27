0205023d8432797d68e5f9a28d9ea62ef7d62a0f
Momoh Nobert <okhai0112@gmail.com>
2020-06-28 00:43:56 +0100

    added function to trim the words, make lower case and print them

--- WordCounter/views.py
+++ WordCounter/views.py
@@ -1,3 +1,5 @@
+import operator
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -8,5 +10,20 @@ def home(request):
 
 def count(request):
     fulltext = request.GET['fulltext']
-    print(fulltext)
-    return render(request, 'count.html')
+    word_list = fulltext.split()
+    word_dictionary = {}
+
+    for word in word_list:
+        word = word.lower()
+        symbols = "!@#$%^&*()_+<>?\":,./;[]-='"
+        for i in range(0, len(symbols)):
+            word = word.replace(symbols[i], "")
+        if len(word) > 0:
+            if word in word_dictionary:
+                word_dictionary[word] += 1
+            else:
+                word_dictionary[word] = 1
+    word_dictionary = sorted(word_dictionary.items(), key=operator.itemgetter(1), reverse=True)
+    return render(request, 'count.html',
+                  {'fulltext': fulltext, 'count': len(word_list), 'word_dictionary': word_dictionary})
+
